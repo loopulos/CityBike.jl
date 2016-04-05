06a7aebab7e7f1400f3f2997efea44b404daaf7f
Martin Zumaya <sstereo.vision@gmail.com>
2016-04-05 18:25:38 -0500

    error, hour was always zero
    turned back to Hour method

--- scripts/par_test_last.jl
+++ scripts/par_test_last.jl
@@ -16,7 +16,7 @@ println(file, "id_start,id_end,month,day,hour,mins")
 for i in 1:size(raw_data,1)
     st_time  = DateTime(Ddata[i,5]*" "*Ddata[i,6], "y-m-d H:M:S.s")
     end_time = DateTime(Ddata[i,8]*" "*Ddata[i,9], "y-m-d H:M:S.s")
-    println(file, Ddata[i,4], ",", Ddata[i,7], ",", Dates.month(st_time), ",", Dates.dayofweek(st_time), ",",Dates.hour(st_time), ",", round(Int, Dates.value(end_time - st_time) / (60*1000)) )
+    println(file, Ddata[i,4], ",", Ddata[i,7], ",", Dates.month(st_time), ",", Dates.dayofweek(st_time), ",",Dates.value(Dates.Hour(st_time)), ",", round(Int, Dates.value(end_time - st_time) / (60*1000)) )
 end
 
 close(file)
