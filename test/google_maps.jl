001617f337242c70f2219bb12ad09eeb0b02324a
spiralizing <spiralizing@gmail.com>
2016-09-01 15:54:48 -0500

    finishing the script and adding the stations

--- test/google_maps.jl
+++ test/google_maps.jl
@@ -1,33 +1,27 @@
 using Requests
-
 key = "AIzaSyCXia5c3mcH4rxdFtLl6qOew0g_W5qOGrE"
 
 # lat, long
-origin = "19.42661,-99.14447"
-
-destination = "19.36065%2C-99.168669"
-
-origin = "19.373262%2C-99.184333"
-destination = "19.371152%2C-99.183034"
 
 mode = ["driving", "bicycling", "transit"]
 traffic_model = "best_guess"
 transit_mode = "subway"
+string(123.242,',',12342.123)
+estaciones = float64([readcsv("estacionesn.csv")[2:end,1] readcsv("estacionesn.csv")[2:end,10:11]])
+ns = size(estaciones)[1]
+durs = zeros(5, int(ns * (ns - 1) / 2))
+for j = 1:(ns-1)
+  for k = (j+1):ns
+    origin = string(estaciones[j,2],",",estaciones[j,3])
+    destination = string(estaciones[k,2],"%2",estaciones[k,3])
+    durs[j,1] = estaciones[j,1]; durs[j,2] = estaciones[k,1]
+    for i in 1:length(mode)
 
-estaciones = readcsv("/Users/martinC3/Google Drive/EcobiciDATA/EcobiciDF/estacionesn.csv")
+      URL = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=$(origin)&destinations=$(destination)&mode=$(mode[i])&key=$(key)"
 
-for i in 1:15
-    println(i, estaciones[1, i])
-end
+      response = Requests.json(get(URL))
+      durs[j,2+i] = response["rows"][1]["elements"][1]["duration"]["value"]
 
-
-durs = zeros(3, )
-
-for i in length(mode):
-
-    URL = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=$(origin)&destinations=$(destination)&mode=$(mode[i])&key=$(key)"
-
-    response = Requests.json(get(URL))
-    response["rows"][1]["elements"][1]["duration"]["value"]
-
+      end
+    end
 end
