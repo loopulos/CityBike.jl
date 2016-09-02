49a4a8b2ded19805a0b66d553a3e5ad349a47bb4
spiralizing <spiralizing@gmail.com>
2016-09-02 10:17:01 -0500

    fixed the google_maps script

--- test/google_maps.jl
+++ test/google_maps.jl
@@ -6,19 +6,24 @@ mode = ["driving", "bicycling", "transit"]
 #transit_mode = "subway"
 estaciones = float64([readcsv("estacionesn.csv")[2:end,1] readcsv("estacionesn.csv")[2:end,10:11]])
 ns = size(estaciones)[1]
+ns = 5
+nt = int(ns * (ns - 1)/2)
 durs = zeros(int(ns * (ns - 1) / 2),5)
+s=1
 for j = 1:(ns-1)
-  for k = (j+1):ns
-    origin = string(estaciones[j,2],",",estaciones[j,3])
-    destination = string(estaciones[k,2],"%2C",estaciones[k,3])
-    durs[j,1] = estaciones[j,1]; durs[j,2] = estaciones[k,1]
-    for i in 1:length(mode)
-      println(j,'\t',k,'\t',i)
-      URL = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=$(origin)&destinations=$(destination)&mode=$(mode[i])&key=$(key)"
+    for k = (j+1):ns
+        origin = string(estaciones[j,2],",",estaciones[j,3])
+        destination = string(estaciones[k,2],"%2C",estaciones[k,3])
+        durs[s,1] = estaciones[j,1]; durs[s,2] = estaciones[k,1]
+        for i in 1:length(mode)
+            #println(j,'\t',k,'\t',i)
+            URL = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=$(origin)&destinations=$(destination)&mode=$(mode[i])&key=$(key)"
 
-      response = Requests.json(get(URL))
-      durs[j,2+i] = response["rows"][1]["elements"][1]["duration"]["value"]
+            response = Requests.json(get(URL))
+            durs[s,2+i] = response["rows"][1]["elements"][1]["duration"]["value"]
 
-      end
+        end
+        s += 1
     end
 end
+durs
