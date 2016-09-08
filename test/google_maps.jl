94c958ead4ca1c6d1a782042d21279465f1673dd
spiralizing <spiralizing@gmail.com>
2016-09-07 19:15:40 -0500

    bleh

--- test/google_maps.jl
+++ test/google_maps.jl
@@ -22,14 +22,14 @@ function manda(args::Array{Int64,1},mode::Array{ASCIIString,1},origin::AbstractS
 end
 ######################ESTA ES LA SECCION DE PRUEBA ########################################################################################################
 # origin = string(estaciones[1,2],",",estaciones[1,3])
-#  destination = string(estaciones[11,2],"%2C",estaciones[11,3])
-#  for i = 2:100
-#      #origin = origin*string("|",estaciones[i,2],",",estaciones[i,3])
-#      destination = destination*string("%7C",estaciones[i,2],"%2C",estaciones[i,3])
-#  end
-#  URL = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=$(origin)&destinations=$(destination)&mode=transit&key=$(key2)"
-#  response = Requests.json(get(URL))
-#  response["rows"][1]["elements"][100]["distance"]["value"]
+# destination = string(estaciones[11,2],"%2C",estaciones[11,3])
+# for i = 2:50
+#       #origin = origin*string("|",estaciones[i,2],",",estaciones[i,3])
+#    destination = destination*string("%7C",estaciones[i,2],"%2C",estaciones[i,3])
+# end
+# URL = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=$(origin)&destinations=$(destination)&mode=transit&key=$(key2)"
+# response = Requests.json(get(URL))
+# response["rows"][1]["elements"][100]["distance"]["value"]
 #####################################################AQUI EMPIEZA #############################################################
 
 usos = int(readcsv("usofilt2_2015.csv"))#el uso de estaciones filtrado
@@ -44,7 +44,7 @@ for j = 1:ns
     lat1 = estaciones[estaciones[:,1].==j,:][2]; long1 = estaciones[estaciones[:,1].==j,:][3] #aqui se define la estacion de inicio (coordenadas)
     origin = string(lat1,",",long1)
     durs[j] = zeros(size(mat)[1], 8)
-    println("Voy con la estacion ", l)
+    println("Voy con la estacion ", j)
     for l = 0:(nit[1]-1) #aqui se van a construir los bloques para los requests
         lat2 = estaciones[estaciones[:,1].==mat[l*sb+1,2],:][2]; long2 = estaciones[estaciones[:,1].==mat[l*sb+1,2],:][3]
         destination = string(lat2,"%2C",long2); durs[j][1,1] = mat[1,1]; durs[j][1,2] = mat[1,2]
@@ -54,7 +54,7 @@ for j = 1:ns
             durs[j][l*sb+i,1] = mat[l*sb+i,1]; durs[j][l*sb+i,2] = mat[l*sb+i,2]
         end
         args = [l,sb]
-        taskQ = Task(() -> manda(args,mode,origin,destination,key,durs[j]))
+        taskQ = Task(() -> manda(args,mode,origin,destination,key2,durs[j]))
         for x in taskQ
         end
 
@@ -68,7 +68,7 @@ for j = 1:ns
         durs[j][nit[1]*sb+i,1] = mat[nit[1]*sb+i,1]; durs[j][nit[1]*sb+i,2] = mat[nit[1]*sb+i,2]
     end
     args = [nit[1],nit[2]]
-    taskQ = Task(() -> manda(args,mode,origin,destination,key,durs[j]))
+    taskQ = Task(() -> manda(args,mode,origin,destination,key2,durs[j]))
     for x in taskQ
     end
 end
