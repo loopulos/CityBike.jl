fa950da58e9b2ee201f3bfc25e384656ed855fb8
Martin Zumaya <sstereo.vision@gmail.com>
2017-07-18 14:22:50 -0500

    filter data per month

--- scripts/matriz_adj.jl
+++ scripts/matriz_adj.jl
@@ -1,46 +1,64 @@
+###================###================###================###================###
+###  GENERA MATRICES DE ADYACENCIA POR MES
+###================###================###================###================###
+
 using Plots
 
 gui()
 gr()
 pyplot()
 
+###================###================###================###================###
+
 data_path = "/$(homedir())/Google\ Drive/EcobiciDATA/EcobiciDF"
-# data_path = "/$(homedir())/Google\ Drive/EcobiciDATA/EcobiciDF"
+data_path = "/$(homedir())/Ecobici"
 
 files = filter(x -> ismatch( r"filt_\d+.csv", x), readdir(data_path))
 
-file = files[end]
+# mes es la columna 3
+data = readcsv(data_path*"/"*files[1])[2:end, :]
 
-data = readcsv(data_path*"/"*file)
+###================###================###================###================###
 
-route = Dict()
+m = 2
+month_data = view(data, find(x->x==m, data[:,3]), 1:2)
 
-for i in 2:length(data[2:end, 1])
-    if data[i, 1] < 500 && data[i, 2] < 500
-        # key = (id_start, id_end)
-        # val = # apariciones
-        val = (data[i, 1], data[i, 2])
-        haskey(route, val) == false ? route[val] = 1 : route[val] += 1
+trip = Dict()
+# key = (id_start, id_end)
+# val = # apariciones
+
+for i in 1:size(month_data,1)
+    val = (month_data[i, 1], month_data[i, 2])
+    if month_data[i, 1] < 500 && month_data[i, 2] < 500 && month_data[i,1] != month_data[i,2]
+        haskey(trip, val) == false ? trip[val] = 1 : trip[val] += 1
     end
 end
 
-id_start = [k[1] for k in keys(route)]
-id_end  = [k[2] for k in keys(route)]
+id_start = [k[1] for k in keys(trip)]
+id_end  = [k[2] for k in keys(trip)]
 
-times_day = 2
+times_month = 2 #numero de veces que se usa (al menos en un mes)
 
-test_line
+vals = filter(x -> x >= times_month, collect(values(trip)))
 
-vals = filter(x -> x >= lw_thr, collect(values(route)))
+# max_time_day = div(maximum(vals), 365)
+# max_times_year = div(sum(collect(values(route))), 365) / 446
 
-max_time_day = div(maximum(vals), 365)
-max_times_year = div(sum(collect(values(route))), 365) / 446
+###================###================###================###================###
 
-histogram(vals, nbins = 50)
+vals = collect(values(route))
+
+histogram(vals, bins = range(minimum(vals)-0.5, maximum(vals)+1), xlims = (0,13), xticks = minimum(vals):maximum(vals), normed = true)
+
+xlims!(0, 15)
+xticks!(collect(1:11))
+
 xlims!((365,maximum(vals)))
 
-
 histogram(filter(x -> x >= threshold, vals), nbins = 50)
+histogram(filter(x -> x >= threshold, vals), nbins = 50)
+
+###================###================###================###================###
 
 adj = spzeros(Float64, maximum(id_start), maximum(id_end))
 adj = zeros(Float64, maximum(id_start), maximum(id_end))
@@ -53,6 +71,8 @@ end
 
 histogram(reshape(adj, 1, length(adj)), nbins = 50)
 
+###================###================###================###================###
+
 out_vals_sort = Float64[out_stations[key] for key in out_keys_sort]
 in_vals_sort  = Float64[in_stations[key] for key in in_keys_sort]
 
@@ -60,6 +80,8 @@ out_tot_counts = sum(Float64[val for val in values(out_stations)])
 in_tot_counts  = sum(Float64[val for val in values(in_stations)])
 
 println(out_tot_counts, "\t", in_tot_counts)
+
+###================###================###================###================###
 
 plt[:plot](out_keys_sort, out_vals_sort ./ out_tot_counts, ".-", label = "out_"*replace(split(data_path*"/"*file, ".")[1], "filt_", ""))
 plt[:plot](in_keys_sort, in_vals_sort ./ in_tot_counts, ".-", label = "in_"*replace(split(data_path*"/"*file, ".")[1], "filt_", ""))
