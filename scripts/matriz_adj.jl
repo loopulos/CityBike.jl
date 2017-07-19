55ce1d2277c8bc308361d936f8e90be33793fd48
Alfredo <spiralizing@gmail.com>
2017-07-18 21:03:36 -0500

    some changes in the estimation of the distributions.

--- scripts/matriz_adj.jl
+++ scripts/matriz_adj.jl
@@ -16,33 +16,62 @@ data_path = "/$(homedir())/Ecobici"
 files = filter(x -> ismatch( r"filt_\d+.csv", x), readdir(data_path))
 
 # mes es la columna 3
-data = readcsv(data_path*"/"*files[1])[2:end, :]
+data = readcsv(data_path*"/"*files[6])[2:end, :]
 
 ###================###================###================###================###
 
-m = 2
-
-for m in 1:12
-month_data = view(data, find(x->x==m, data[:,3]), 1:2)
+m = 10
+# anim = @animate for m in 2:12
+# for m in 2:12
+    # print(m)
 
 trip = Dict()
-# key = (id_start, id_end)
-# val = # apariciones
 
-for i in 1:size(month_data,1)
-    val = (month_data[i, 1], month_data[i, 2])
-    if month_data[i, 1] < 500 && month_data[i, 2] < 500 && month_data[i,1] != month_data[i,2]
-        haskey(trip, val) == false ? trip[val] = 1 : trip[val] += 1
+month_data = view(data, find(x -> x == m, data[:,3]), 1:2)
+
+if length(month_data) != 0.
+    # key = (id_start, id_end)
+    # val = # apariciones
+
+    for i in 1:size(month_data,1)
+        val = (month_data[i, 1], month_data[i, 2])
+        if month_data[i, 1] < 500 && month_data[i, 2] < 500 && month_data[i,1] != month_data[i,2]
+            haskey(trip, val) == false ? trip[val] = 1 : trip[val] += 1
+        end
     end
+
+    # vals = extrema(unique(collect(values(trip))))
+    # println(m, "\t", extrema(unique(collect(values(trip)))), "\t", sum(collect(values(trip))))
+
+    # histogram(vals, bins = range(minimum(vals)-0.5, maximum(vals)+1), xlims = (0,maximum(vals)+4), xticks = minimum(vals):maximum(vals), normed = true)
 end
 
-vals = collect(values(trip))
+# end
 
-histogram(vals, bins = range(minimum(vals)-0.5, maximum(vals)+1), xlims = (0,13), xticks = minimum(vals):maximum(vals), normed = true)
+i_st = [repr(collect(keys(trip))[i][1]) for i in 1:length(collect(keys(trip)))]
+e_st = [repr(collect(keys(trip))[i][2]) for i in 1:length(collect(keys(trip)))]
+
+heatmap(i_st, e_st, transpose(collect(values(trip))), aspect_ratio = 1)
+
+sorted_vals = sort(collect(trip), by = tuple -> last(tuple), rev = true)
+sorted_vals[1][2]
+
+filt_sorted_vals = filter(x -> x[2] > 14, sorted_vals)
+
+bar([ repr(val[1]) for val in filt_sorted_vals], [ val[2] for val in filt_sorted_vals])
+
+bar(collect(1:length(sorted_vals)), [ val[2] for val in sorted_vals], xscale = :log10, yscale = :log10)
+
+scatter(collect(1:length(sorted_vals)), [ val[2] for val in sorted_vals], yscale = :log10, xlims = (1, length(sorted_vals)))
+
+scatter(collect(1:length(sorted_vals)), [ val[2] for val in sorted_vals], xscale = :log10, yscale = :log10, xlims = (1, length(sorted_vals)))
+
+gui()
+
+gif(anim, "distros.gif", fps = 2)
+###================###================###================###================###
 
 # histogram(filter(x -> x >= threshold, vals), nbins = 50)
-end
-###================###================###================###================###
 
 id_start = [k[1] for k in keys(trip)]
 id_end  = [k[2] for k in keys(trip)]
